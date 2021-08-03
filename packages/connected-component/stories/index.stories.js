cadb60bbdb0e075d4541ac6bec9edfc149992af3
Prasath Muthaiyan <prasath.muthaiyan@gmail.com>
2021-08-03 11:09:35 +0530

    fix: json parse issue

--- packages/connected-component/stories/index.stories.js
+++ packages/connected-component/stories/index.stories.js
@@ -9,12 +9,12 @@ export default {
 const list = [
   {
     name: 'Mukesh Ambani 1',
-    icon: 'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5c7d7829a7ea434b351ba0b6%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D206%26cropX2%3D2043%26cropY1%3D250%26cropY2%3D2089',
+    icon: 'ok',
     msg: 'sample',
   },
   {
     name: 'Mukesh Ambani 2',
-    icon: 'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5c7d7829a7ea434b351ba0b6%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D206%26cropX2%3D2043%26cropY1%3D250%26cropY2%3D2089',
+    icon: 'ok',
     msg: 'sample',
   },
 ];
